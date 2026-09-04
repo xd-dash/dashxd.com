cb7aaa26b41dcf3cdb2045408cdbcc9274022951
dev@dashxd <125025267+dash-xd@users.noreply.github.com>
2026-09-04 01:51:00 -0700

    Expose Cloudflare request metadata

--- worker.js
+++ worker.js
@@ -1,4 +1,17 @@
 const HTML = __HURAM_INLINE_HTML__;
+const SERVICE = "dashxd-com";
+
+function json(data, init = {}) {
+  return new Response(JSON.stringify(data), {
+    ...init,
+    headers: {
+      "content-type": "application/json; charset=utf-8",
+      "cache-control": "no-store",
+      "x-content-type-options": "nosniff",
+      ...(init.headers || {}),
+    },
+  });
+}
 
 export default {
   async fetch(request) {
@@ -14,6 +27,18 @@ export default {
       return new Response("Not Found", { status: 404 });
     }
 
+    if (url.pathname === "/meta") {
+      const cf = request.cf || {};
+      return json({
+        service: SERVICE,
+        colo: cf.colo || null,
+        country: cf.country || null,
+        httpProtocol: cf.httpProtocol || null,
+        tlsVersion: cf.tlsVersion || null,
+        ray: request.headers.get("cf-ray"),
+      });
+    }
+
     if (url.pathname !== "/") {
       return new Response("Not Found", { status: 404 });
     }
@@ -23,8 +48,8 @@ export default {
       headers: {
         "content-type": "text/html; charset=utf-8",
         "cache-control": "public, max-age=300",
-        "x-content-type-options": "nosniff"
-      }
+        "x-content-type-options": "nosniff",
+      },
     });
   },
 };
